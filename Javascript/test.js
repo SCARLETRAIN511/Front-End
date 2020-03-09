a0fe6e95fca33a13a03f7b19cdf66b0e32bbed14
Jiaxuan Tang <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-03-09 01:57:49 +0000

    1

---
+++ Javascript/test.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const nemo = ['nemo'];
+const large = new Array(10).fill('nemo');
+function findNemo(array){
+    let t0 = 10;
+    for (let i=0; i < array.length; i++){
+        if (array[i] === 'nemo'){
+            console.log('Found Nemo');
+        }
+    }
+    let t1 = 20;
+    console.log('call to find nemo took' + (t1-t0) + 'ms');
+}
+findNemo(large);
