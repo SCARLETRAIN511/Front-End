58c48ca8991e6a535e6a67d08d0df4536a73266e
Jiaxuan Tang <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-03-15 18:50:04 +0000

    1

--- Javascript/test.js
+++ Javascript/test.js
@@ -12,6 +12,23 @@ function findNemo(array){
     let t1 = 20;
     console.log('call to find nemo took' + (t1-t0) + 'ms');
 }
-findNemo(large);
 
-console.log(typeof NaN)
+function Test1(){
+    var A1 = "Javascript";
+    console.log(A1[0]);
+    console.log(A1[9]);
+    console.log(A1.length);
+
+    let k;
+    k=20
+    console.log(k);
+
+}
+
+function calculater(year){
+    alert("this is roughly " + year*365 + "days");
+}
+
+
+let var1 =prompt("how old are u");
+calculater(var1);
