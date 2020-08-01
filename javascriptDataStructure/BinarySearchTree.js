3c26a21d8e426df2c1bcc2932e613aea19819615
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-08-01 17:27:29 +0800

    Create BinarySearchTree.js

---
+++ javascriptDataStructure/BinarySearchTree.js
@@ -0,0 +1,42 @@
+//javascript
+
+class TreeNode {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree{
+    constructor() {
+        this.root = null;
+    }
+
+    addNode(data){
+        const newNode = new TreeNode(data);
+        if (!this.root){
+            this.root = newNode;
+        }
+        else {
+            this.insertNode(this.root,newNode);
+        }
+    }
+
+    insertNode(node,newNode){
+        if (newNode.data <= node.data){
+            if (!node.left){
+                node.left = new;
+            }else{
+                this.insertNode(node.left,newNode);
+            }
+        }else {
+            if (!node.right) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
+    }
+}
+
