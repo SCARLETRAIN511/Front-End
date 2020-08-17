5aaa696e62cf60fa431570ed8ee0e65313b60c03
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-08-17 15:55:25 +0800

    Update BubbleSort.js

--- javascriptDataStructureAlgorithms/BubbleSort.js
+++ javascriptDataStructureAlgorithms/BubbleSort.js
@@ -16,4 +16,18 @@ function bubbleSort(array){
     return array
 }
 
-console.log(bubbleSort([5,3,25,7,2,3]));
+//second way of implementation
+function bubbleSort2(array){
+    for (let i = 0;i < array.length;i++){
+        for (let j = i;j<array.length;j++){
+            if (array[i] > array[j]){
+                let temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+    return array
+}
+
+console.log(bubbleSort2([5,3,25,7,2,3]));
